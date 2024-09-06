7d2743b5cddf9b9597fce4bcc921f10683c5a391
jean.dominguez <jean.dominguez@btilab.co>
2024-09-06 15:04:35 -0500

    feat: new icons and images for skills

--- src/screens/Skills/Skills.tsx
+++ src/screens/Skills/Skills.tsx
@@ -1,17 +1,15 @@
 import { Text } from "@/components"
+import { IMAGES } from "@/Constants"
 import {
 	FaBootstrap,
 	FaCss3,
-	FaFigma,
 	FaGithub,
 	FaHtml5,
 	FaJira,
 	FaReact,
-	FaSlack,
 } from "react-icons/fa"
 import { RiNextjsLine, RiTailwindCssFill } from "react-icons/ri"
 import {
-	SiClickup,
 	SiExpress,
 	SiJavascript,
 	SiPostman,
@@ -21,7 +19,7 @@ import {
 import { TbBrandReactNative } from "react-icons/tb"
 export const Skills = () => {
 	return (
-		<div className="flex flex-col gap-y-10 px-5 py-4">
+		<div className="flex flex-col gap-y-5 px-5 py-4">
 			<Text type="h1" className="font-sans dark:text-gray-500 text-black">
 				Skills
 			</Text>
@@ -68,15 +66,7 @@ export const Skills = () => {
 						ReactJS
 					</Text>
 				</div>
-				<div className="flex  items-center gap-6">
-					<TbBrandReactNative
-						className="text-extensions-jsx"
-						size={48}
-					/>
-					<Text type="p" className="font-sans dark:text-white">
-						React Native
-					</Text>
-				</div>
+
 				<div className="flex  items-center gap-6">
 					<RiTailwindCssFill
 						className="text-extensions-ts"
@@ -121,13 +111,13 @@ export const Skills = () => {
 					</Text>
 				</div>
 				<div className="flex  items-center gap-6">
-					<FaSlack className="text-pink-500" size={48} />
+					<img src={IMAGES.slack} />
 					<Text type="p" className="font-sans dark:text-white">
 						Slack
 					</Text>
 				</div>
 				<div className="flex  items-center gap-6">
-					<SiClickup className="dark:text-white" size={48} />
+					<img src={IMAGES.clickUp} width={48} height={48} />
 					<Text type="p" className="font-sans dark:text-white">
 						Click up
 					</Text>
@@ -139,7 +129,7 @@ export const Skills = () => {
 					</Text>
 				</div>
 				<div className="flex  items-center gap-6">
-					<FaFigma className="dark:text-white" size={48} />
+					<img src={IMAGES.figma} />
 					<Text type="p" className="font-sans dark:text-white">
 						Figma
 					</Text>
@@ -164,48 +154,60 @@ export const Skills = () => {
 			</div>
 			<div className="lg:flex lg:flex-row lg:gap-x-10 flex-col">
 				<div className="flex  items-center gap-6">
-					<SiVisualstudiocode
+					<img
+						src={IMAGES.axios}
+						width={70}
+						height={70}
+						className="text-extensions-jsx"
+					/>
+					<Text type="p" className="font-sans dark:text-white">
+						Axios
+					</Text>
+				</div>
+				<div className="flex  items-center gap-6">
+					<img src={IMAGES.reactDom} width={48} height={48} />
+					<Text type="p" className="font-sans dark:text-white">
+						React Router Dom
+					</Text>
+				</div>
+				<div className="flex  items-center gap-6">
+					<img src={IMAGES.zustands} width={70} height={70} />
+					<Text type="p" className="font-sans dark:text-white">
+						Zustand
+					</Text>
+				</div>
+				<div className="flex  items-center gap-6">
+					<img src={IMAGES.Redux} width={50} height={50} />
+					<Text type="p" className="font-sans dark:text-white">
+						Redux
+					</Text>
+				</div>
+				<div className="flex  items-center gap-6">
+					<img src={IMAGES.mui} width={50} height={50} />
+					<Text type="p" className="font-sans dark:text-white">
+						Material Ui
+					</Text>
+				</div>
+			</div>
+			<div className="py-4">
+				<Text type="span" className="font-sans dark:text-gray-500">
+					Mobile
+				</Text>
+			</div>
+			<div className="lg:flex lg:flex-row lg:gap-x-10 flex-col">
+				<div className="flex  items-center gap-6">
+					<TbBrandReactNative
 						className="text-extensions-jsx"
 						size={48}
 					/>
 					<Text type="p" className="font-sans dark:text-white">
-						Visual studio
+						React Native
 					</Text>
 				</div>
 				<div className="flex  items-center gap-6">
-					<FaSlack className="text-pink-500" size={48} />
+					<img src={IMAGES.flutter} width={50} height={50} />
 					<Text type="p" className="font-sans dark:text-white">
-						Slack
-					</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<SiClickup className="dark:text-white" size={48} />
-					<Text type="p" className="font-sans dark:text-white">
-						Click up
-					</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<FaJira className="text-extensions-css" size={48} />
-					<Text type="p" className="font-sans dark:text-white">
-						Jira
-					</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<FaFigma className="dark:text-white" size={48} />
-					<Text type="p" className="font-sans dark:text-white">
-						Figma
-					</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<SiPostman className="text-orange-500" size={48} />
-					<Text type="p" className="font-sans dark:text-white">
-						Postman
-					</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<FaGithub className="dark:text-white" size={48} />
-					<Text type="p" className="font-sans dark:text-white">
-						Git
+						Flutter
 					</Text>
 				</div>
 			</div>
