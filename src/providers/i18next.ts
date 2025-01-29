bb98463b1aeb2183679d22b1be4afb73c78a163f
jean dominguez <jeansi123@live.com>
2025-01-29 15:13:43 -0500

    feat: new screen in translate

--- src/providers/i18next.ts
+++ src/providers/i18next.ts
@@ -5,10 +5,12 @@ import { initReactI18next } from "react-i18next"
 import experienceEN from "@/translations/en/experience.json"
 import layoutEN from "@/translations/en/layout.json"
 import projectsEN from "@/translations/en/projects.json"
+import skillsEN from "@/translations/en/skills.json"
 // LANGUAGE ES
 import experienceES from "@/translations/es/experience.json"
 import layoutES from "@/translations/es/layout.json"
 import projectsES from "@/translations/es/projects.json"
+import skillsES from "@/translations/es/skills.json"
 
 translater
 	.use(LanguageDetector)
@@ -19,11 +21,13 @@ translater
 				layout: layoutES,
 				experience: experienceES,
 				projects: projectsES,
+				skills: skillsES,
 			},
 			en: {
 				layout: layoutEN,
 				experience: experienceEN,
 				projects: projectsEN,
+				skills: skillsEN,
 			},
 		},
 		lng: "en",
