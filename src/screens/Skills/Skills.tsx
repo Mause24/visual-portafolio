5f6873b371320516ba85aa122dc50e85041a6c66
jean dominguez <jeansi123@live.com>
2025-01-30 16:20:52 -0500

    feat: refactor in skills screen

--- src/screens/Skills/Skills.tsx
+++ src/screens/Skills/Skills.tsx
@@ -2,189 +2,39 @@ import { Text } from "@/components"
 import { Category } from "./Skills.types"
 import { useSkills } from "./useSkills"
 export const Skills = (): JSX.Element => {
-	const { skillsCategories } = useSkills()
-	console.log("buenas", skillsCategories)
+	const { skillsCategories, skillsTitle } = useSkills()
 
 	return (
-		<div className="flex flex-col gap-y-5 px-5 py-4 max-h-screen overflow-y-auto">
-			<Text type="h2" className="font-sans dark:text-gray-500 text-black">
-				Skills
+		<div className=" px-5 py-4 max-h-screen overflow-y-auto">
+			<Text type="h2" className="font-sans dark:text-white text-black">
+				{skillsTitle}
 			</Text>
-			<div className="py-4">
-				<Text type="h3" className="font-sans dark:text-gray-500">
-					Languages
-				</Text>
-			</div>
-			<div className="gap-y-4">
-				{skillsCategories.map((category: Category) => (
-					<div key={category.id}>
-						<h2>{category.label}</h2>
-						<ul>
-							<div className="flex gap-x-4">
-								{category.items.map(item => (
-									<li key={item.label}>
-										<div className="flex items-center gap-4">
-											<img
-												className="h-12 w-12"
-												src={item.image}
-												alt={item.label}
-											/>
-											<span>{item.label}</span>
-										</div>
-									</li>
-								))}
-							</div>
-						</ul>
-					</div>
-				))}
 
-				{/* <div className="flex  items-center gap-6">
-					<SiJavascript className="text-extensions-js" size={48} />
-					<Text className="font-sans dark:text-white">
-						JavaScript
-					</Text>
+			{skillsCategories.map((category: Category) => (
+				<div key={category.id}>
+					<h2 className="py-4 dark:text-white text-black">
+						{category.label}
+					</h2>
+					<ul>
+						<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-4 w-full h-full">
+							{category.items.map(item => (
+								<li key={item.label}>
+									<div className="flex items-center gap-x-4">
+										<img
+											className="h-12 w-12"
+											src={item.image}
+											alt={item.label}
+										/>
+										<span className="dark:text-white text-black">
+											{item.label}
+										</span>
+									</div>
+								</li>
+							))}
+						</div>
+					</ul>
 				</div>
-				<div className="flex  items-center gap-6">
-					<SiTypescript className="text-extensions-ts" size={48} />
-					<Text className="font-sans dark:text-white">
-						TypeScript
-					</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<FaHtml5 className="text-extensions-html" size={48} />
-					<Text className="font-sans dark:text-white">HTML</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<FaCss3 className="text-extensions-css" size={48} />
-					<Text className="font-sans dark:text-white">CSS3</Text>
-				</div> */}
-			</div>
-			{/* <div className="py-4">
-				<Text type="h3" className="font-sans dark:text-gray-500">
-					Frameworks
-				</Text>
-			</div>
-			<div className="lg:flex lg:flex-row lg:gap-x-10 flex flex-col gap-y-5">
-				<div className="flex  items-center gap-6">
-					<FaReact className="text-extensions-jsx" size={48} />
-					<Text className="font-sans dark:text-white">ReactJS</Text>
-				</div>
-
-				<div className="flex  items-center gap-6">
-					<RiTailwindCssFill
-						className="text-extensions-ts"
-						size={48}
-					/>
-					<Text className="font-sans dark:text-white">
-						Tailwind css
-					</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<RiNextjsLine className="dark:text-white" size={48} />
-					<Text className="font-sans dark:text-white">NextJS</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<SiExpress className="text-extensions-css" size={48} />
-					<Text className="font-sans dark:text-white">ExpressJS</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<FaBootstrap className="text-purple-800" size={48} />
-					<Text className="font-sans dark:text-white">Boostrap</Text>
-				</div>
-			</div>
-			<div className="py-4">
-				<Text type="h3" className="font-sans dark:text-gray-500">
-					Tools
-				</Text>
-			</div>
-			<div className="lg:flex lg:flex-row lg:gap-x-10 flex flex-col gap-y-5">
-				<div className="flex  items-center gap-6">
-					<img src={IMAGES.visual} className="h-12 w-12" />
-					<Text className="font-sans dark:text-white">
-						Visual studio
-					</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<img src={IMAGES.slack} />
-					<Text className="font-sans dark:text-white">Slack</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<img src={IMAGES.clickUp} width={48} height={48} />
-					<Text className="font-sans dark:text-white">Click up</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<FaJira className="text-extensions-css" size={48} />
-					<Text className="font-sans dark:text-white">Jira</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<img src={IMAGES.figma} />
-					<Text className="font-sans dark:text-white">Figma</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<SiPostman className="text-orange-500" size={48} />
-					<Text className="font-sans dark:text-white">Postman</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<FaGithub className="dark:text-white" size={48} />
-					<Text className="font-sans dark:text-white">Git</Text>
-				</div>
-			</div>
-			<div className="py-4">
-				<Text type="h3" className="font-sans dark:text-gray-500">
-					libraries
-				</Text>
-			</div>
-			<div className="lg:flex lg:flex-row lg:gap-x-10 flex flex-col gap-y-5 ">
-				<div className="flex  items-center gap-6">
-					<img
-						src={IMAGES.axios}
-						width={70}
-						height={70}
-						className="text-extensions-jsx"
-					/>
-					<Text className="font-sans dark:text-white">Axios</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<img src={IMAGES.reactDom} width={48} height={48} />
-					<Text className="font-sans dark:text-white">
-						React Router Dom
-					</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<img src={IMAGES.zustands} width={70} height={70} />
-					<Text className="font-sans dark:text-white">Zustand</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<img src={IMAGES.Redux} width={50} height={50} />
-					<Text className="font-sans dark:text-white">Redux</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<img src={IMAGES.mui} width={50} height={50} />
-					<Text className="font-sans dark:text-white">
-						Material Ui
-					</Text>
-				</div>
-			</div>
-			<div className="py-4">
-				<Text type="h3" className="font-sans dark:text-gray-500">
-					Mobile
-				</Text>
-			</div>
-			<div className="lg:flex lg:flex-row lg:gap-x-10 flex flex-col gap-y-5">
-				<div className="flex  items-center gap-6">
-					<TbBrandReactNative
-						className="text-extensions-jsx"
-						size={48}
-					/>
-					<Text className="font-sans dark:text-white">
-						React Native
-					</Text>
-				</div>
-				<div className="flex  items-center gap-6">
-					<img src={IMAGES.flutter} width={50} height={50} />
-					<Text className="font-sans dark:text-white">Flutter</Text>
-				</div>
-			</div> */}
+			))}
 		</div>
 	)
 }
